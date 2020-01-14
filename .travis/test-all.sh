4afbba8c254f5d6a8e90b0ffb49a943342680f62
Grigory Pomadchin <gr.pomadchin@gmail.com>
2020-01-14 15:00:21 -0500

    Fix test-all scripts

--- .travis/test-all.sh
+++ .travis/test-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ `echo $TRAVIS_SCALA_VERSION | cut -f1-2 -d "."` = "2.13" ]; then
+if [[ `echo $TRAVIS_SCALA_VERSION | cut -f1-2 -d "."` = "2.13" ]]; then
     .travis/test.sh;
 else
     .travis/test-212.sh;
