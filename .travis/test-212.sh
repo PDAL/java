9e6ed14467f62e3395e322494c63ad8eddc49275
Grigory Pomadchin <gr.pomadchin@gmail.com>
2020-03-26 09:34:36 -0400

    Cache Homebrew

--- .travis/test-212.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-./sbt "-212" "project core" test || { exit 1; }
-./sbt "-212" "project core-scala" test || { exit 1; }
-
-cd examples/pdal-jni
-./sbt "-212" "runMain com.azavea.Main" || { exit 1; }
-./sbt "-212" "runMain com.azavea.MainScala" || { exit 1; }
-cd ~-
--- .travis/test-all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ `echo $TRAVIS_SCALA_VERSION | cut -f1-2 -d "."` = "2.13" ]]; then
-    .travis/test.sh;
-else
-    .travis/test-212.sh;
-fi
